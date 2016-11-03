3f6daec703876b6f4ff78191f29b514fed72fabb
makaimark <makaimark@gmail.com>
2016-11-03 13:44:14 +0100

    i can click on the tiles

--- src/GameArea.java
+++ src/GameArea.java
@@ -3,6 +3,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -14,9 +15,9 @@ import javafx.stage.Stage;
 public class GameArea extends Application {
 
     public static final int TILE_SIZE = 20;
-    private static final int TILES_PER_ROW = 10;
-    private static final int TILES_PER_COL = 10;
-    private Tile[][] grid = new Tile[TILES_PER_ROW][TILES_PER_COL];
+    public static final int TILES_PER_ROW = 10;
+    public static final int TILES_PER_COL = 10;
+    private static Tile[][] grid = new Tile[TILES_PER_ROW][TILES_PER_COL];
 
     /*
      * Create the Tiles, put them in the matrix and append them to the Pane one by one
@@ -25,11 +26,27 @@ public class GameArea extends Application {
     private Parent createContent() {
         Pane root = new Pane();
         root.setPrefSize(TILES_PER_ROW * TILE_SIZE, TILES_PER_COL * TILE_SIZE);
+
         for (int x = 0; x < TILES_PER_ROW; x++) {
             for (int y = 0; y < TILES_PER_COL; y++) {
                 Tile tile = new Tile(x, y, Math.random() < 0.2);
+                //tile.addEventHandler(MouseEvent.MOUSE_CLICKED, new ClickOnTileActionListener());
                 grid[x][y] = tile;
                 root.getChildren().add(grid[x][y]);
+            }
+        }
+
+        for (int x = 0; x < TILES_PER_ROW; x++) {
+            for (int y = 0; y < TILES_PER_COL; y++) {
+                Tile tile = grid[x][y];
+
+                if ( tile.hasBomb()) {
+                    continue;
+                }
+
+                long numberOfBombs = tile.getNeighbours(tile).stream().filter(actualTile -> actualTile.hasBomb()).count();
+                tile.setBombsAround((int)numberOfBombs);
+                //tile.text.setText(String.valueOf(numberOfBombs));
             }
         }
         return root;
@@ -37,6 +54,10 @@ public class GameArea extends Application {
 
     public static void main(String[] args){
         launch(args);
+    }
+
+    static Tile getTileFromGrid(int x, int y) {
+        return grid[x][y];
     }
 
     /*
--- src/Tile.java
+++ src/Tile.java
@@ -1,23 +1,33 @@
+import javafx.event.EventHandler;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.sun.java.accessibility.util.AWTEventMonitor.addMouseListener;
 
 
 /**
  * Created by milu_laci on 02.11.16.
  */
 public class Tile extends StackPane {
-    int x, y;
-    boolean hasBomb;
+    private int x, y;
+    private boolean hasBomb;
+    private int bombsAround;
 
     private Rectangle border = new Rectangle(GameArea.TILE_SIZE - 2, GameArea.TILE_SIZE - 2);
-    private Text text = new Text();
+    public Text text = new Text();
 
     public Tile (int x, int y, boolean hasBomb) {
         this.x = x;
         this.y = y;
         this.hasBomb = hasBomb;
+
 
         // Fill the Tile with color
         border.setFill(Color.WHITE);
@@ -29,7 +39,7 @@ public class Tile extends StackPane {
         text.setFill(Color.BLACK);
 
         // If there is a bomb, print X
-        text.setText(hasBomb ? "X" : "");
+        //text.setText(hasBomb ? "X" : "");
 
         // Append the text and the border to the Pane
         getChildren().addAll(border, text);
@@ -37,5 +47,44 @@ public class Tile extends StackPane {
         // This part is responsible for the matrix form
         setTranslateX(x * GameArea.TILE_SIZE);
         setTranslateY(y * GameArea.TILE_SIZE);
+
+        border.setOnMouseClicked(t -> border.setFill(Color.RED));
+
+    }
+
+    List<Tile> getNeighbours(Tile tile) {
+        List <Tile> neighbours = new ArrayList<>();
+        int[] indicies = new int[] {
+                -1, -1,
+                -1, 0,
+                -1, 1,
+                0, -1,
+                0, 1,
+                1, -1,
+                1, 0,
+                1, 1
+        };
+
+        for ( int i = 0; i < indicies.length; i++ ) {
+            int newX = tile.x + indicies[i];
+            int newY = tile.y + indicies[++i];
+
+            if ( newX < GameArea.TILES_PER_ROW && newX >= 0 && newY >= 0 && newY < GameArea.TILES_PER_COL) {
+                neighbours.add(GameArea.getTileFromGrid(newX, newY));
+            }
+        }
+        return neighbours;
+    }
+
+    boolean hasBomb() {
+        return this.hasBomb;
+    }
+
+    void setBombsAround( int numberOfBombs) {
+        this.bombsAround = numberOfBombs;
+    }
+
+    int getBombsAround() {
+        return this.bombsAround;
     }
 }
